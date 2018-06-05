a8b58d7c3b4509d29fcffd191735002eab6461ad
Justin Myers <jjj@justinmyers.com>
2018-06-05 08:25:08 -0400

    Final version 1.0

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,68 @@
 
 const API_KEY = 'xu1XZQwBXKXaU296EbB91f2VNViUUKcf';
+let searchText = "";
+let limit = 8;
 
-
-//let imageDivs = [...document.querySelectorAll('.image-img')];
+//DOM Elements
 let imageGallery = document.querySelector('.image-gallery');
-document.getElementById('submit').addEventListener('click', loadTrending);
+let searchField = document.getElementById('search-term');
+let loadMore = document.querySelector('.load-more');
+
+document.getElementById('submit').addEventListener('click', loadSearch);
+loadMore.addEventListener('click', loadMoreGIFS);
+
+window.addEventListener('load', loadTrending);
 
 
+//Get Data Functions
 function loadTrending(event) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://api.giphy.com/v1/gifs/trending?api_key=' + API_KEY), true;
+    let URL = 'http://api.giphy.com/v1/gifs/trending?api_key=' + API_KEY;
+    getGIFS(URL);
+}
+
+
+function loadSearch(event) {
+    event.preventDefault();
+
+    if (searchField.value === "") return;
+
+    loadMore.style.display = "block";
+
+    clearElement(imageGallery);
+    limit = 8;
+
+    searchText = searchField.value.split(" ").join("+").toLowerCase().trim();
+    let URL = 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=' + API_KEY + '&limit=' + limit;
+    searchField.value = "";
+    getGIFS(URL);
+}
+
+
+function loadMoreGIFS(event) {
+    event.preventDefault();
+    clearElement(imageGallery);
+    
+    limit +=  8;
+    if(limit >= 25) {
+        limit = 25;
+        loadMore.style.display = "none"; 
+    }
+    
+    let URL = 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=' + API_KEY + '&limit=' + limit;
+    searchField.value = "";
+    getGIFS(URL);
+}
+
+
+function getGIFS(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
 
     xhr.onload = function () {
         if (this.status == 200) {
             let gifData = JSON.parse(this.responseText);
-            
+
             for (let i = 0; i < gifData.data.length; i++) {
                 generateImages(gifData.data[i].images.downsized_large.url);
             }
@@ -22,11 +70,12 @@ function loadTrending(event) {
     }
 
     xhr.send();
-    event.preventDefault();
 }
 
 
+//Helper Functions
 function generateImages(url) {
+
     let div = document.createElement("div");
 
     div.className = "image-img";
@@ -36,4 +85,11 @@ function generateImages(url) {
     div.style.backgroundSize = "cover";
 
     imageGallery.appendChild(div);
+}
+
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
 }
