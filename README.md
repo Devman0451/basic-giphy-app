e76207d46bede1e8ed63e4cae5299a134dceccf7
Justin Myers <jjj@justinmyers.com>
2018-08-25 02:31:45 -0400

    Final commit to push

---
+++ README.md
@@ -0,0 +1,3 @@
+#Basic Giphy App
+
+You need to add a giphy API key into the code in order for the app to work.
--- scripts/app.js
+++ scripts/app.js
@@ -1,95 +1,98 @@
 
-const API_KEY = 'xu1XZQwBXKXaU296EbB91f2VNViUUKcf';
-let searchText = "";
-let limit = 8;
+(() => {
+    const API_KEY = /* INSERT API KEY  */;
+    let searchText = "";
+    let limit = 8;
 
-//DOM Elements
-let imageGallery = document.querySelector('.image-gallery');
-let searchField = document.getElementById('search-term');
-let loadMore = document.querySelector('.load-more');
+    // DOM Elements
+    let imageGallery = document.querySelector(".image-gallery");
+    let searchField = document.getElementById("search-term");
+    let loadMore = document.querySelector(".load-more");
 
-document.getElementById('submit').addEventListener('click', loadSearch);
-loadMore.addEventListener('click', loadMoreGIFS);
+    document.getElementById("submit").addEventListener("click", loadSearch);
+    loadMore.addEventListener("click", loadMoreGIFS);
 
-window.addEventListener('load', loadTrending);
-
-
-//Get Data Functions
-function loadTrending(event) {
-    let xhr = new XMLHttpRequest();
-    let URL = 'http://api.giphy.com/v1/gifs/trending?api_key=' + API_KEY;
-    getGIFS(URL);
-}
+    window.addEventListener("load", loadTrending);
 
 
-function loadSearch(event) {
-    event.preventDefault();
-
-    if (searchField.value === "") return;
-
-    loadMore.style.display = "block";
-
-    clearElement(imageGallery);
-    limit = 8;
-
-    searchText = searchField.value.split(" ").join("+").toLowerCase().trim();
-    let URL = 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=' + API_KEY + '&limit=' + limit;
-    searchField.value = "";
-    getGIFS(URL);
-}
-
-
-function loadMoreGIFS(event) {
-    event.preventDefault();
-    clearElement(imageGallery);
-    
-    limit +=  8;
-    if(limit >= 25) {
-        limit = 25;
-        loadMore.style.display = "none"; 
+    // Get Data Functions
+    function loadTrending() {
+        let URL = "http://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY;
+        getGIFS(URL);
     }
-    
-    let URL = 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=' + API_KEY + '&limit=' + limit;
-    searchField.value = "";
-    getGIFS(URL);
-}
 
 
-function getGIFS(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    function loadSearch(event) {
+        event.preventDefault();
 
-    xhr.onload = function () {
-        if (this.status == 200) {
-            let gifData = JSON.parse(this.responseText);
+        if (searchField.value === "") return;
 
-            for (let i = 0; i < gifData.data.length; i++) {
-                generateImages(gifData.data[i].images.downsized_large.url);
+        loadMore.style.display = "block";
+
+        clearElement(imageGallery);
+        limit = 8;
+
+        searchText = searchField.value.split(" ").join("+").toLowerCase().trim();
+        createURL(searchText)
+    }
+
+
+    function loadMoreGIFS(event) {
+        event.preventDefault();
+        clearElement(imageGallery);
+
+        limit += 8;
+        if (limit >= 25) {
+            limit = 25;
+            loadMore.style.display = "none";
+        }
+
+        createURL(searchText)
+    }
+
+    function createURL(searchText) {
+        let URL = `http://api.giphy.com/v1/gifs/search?q=${searchText}&api_key=${API_KEY}&limit=${limit}`;
+        searchField.value = "";
+        getGIFS(URL);
+    }
+
+
+    function getGIFS(url) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+
+        xhr.onload = function () {
+            if (this.status == 200) {
+                let gifData = JSON.parse(this.responseText);
+
+                for (let i = 0; i < gifData.data.length; i++) {
+                    generateImages(gifData.data[i].images.downsized_large.url);
+                }
             }
         }
+
+        xhr.send();
     }
 
-    xhr.send();
-}
 
+    // Helper Functions
+    function generateImages(url) {
 
-//Helper Functions
-function generateImages(url) {
+        let div = document.createElement("div");
 
-    let div = document.createElement("div");
+        div.className = "image-img";
+        div.style.background = "url(" + url + ")";
+        div.style.backgroundRepeat = "no-repeat";
+        div.style.backgroundPosition = "center";
+        div.style.backgroundSize = "cover";
 
-    div.className = "image-img";
-    div.style.background = "url(" + url + ")";
-    div.style.backgroundRepeat = "no-repeat";
-    div.style.backgroundPosition = "center";
-    div.style.backgroundSize = "cover";
-
-    imageGallery.appendChild(div);
-}
-
-
-function clearElement(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+        imageGallery.appendChild(div);
     }
-}
+
+
+    function clearElement(element) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    }
+})();
